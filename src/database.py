18b8a069660430c790d0a6991db1ae6eafaadb13
Reques6e <voronin99borja61275y@rambler.ru>
2024-06-22 21:51:17 +0300

    Обновил db под новую модель и добавил user_create

--- src/database.py
+++ src/database.py
@@ -1,5 +1,7 @@
 import aiosqlite
 
+from auth.models import UserCreate
+
 
 class DataBase:
     def __init__(self) -> None:
@@ -14,6 +16,24 @@ class DataBase:
                     logo TEXT
                 )
             ''')
+
+            await db.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT,
+                    age INTEGER,
+                    password TEXT,
+                    admin INTEGER DEFAULT 0,
+                    block INTEGER DEFAULT 0,
+                    user_group TEXT,
+                    city INTEGER,
+                    address TEXT,
+                    phone INTEGER,
+                    reg_date INTEGER,
+                    passport TEXT
+                )
+            ''')
+
             await db.commit()
 
     async def get_description_info(self) -> str:
@@ -26,7 +46,26 @@ class DataBase:
                 return row[0]
             return None
         
-    async def user_create(
-        self
-    ) -> None:
-        ... 
+    async def user_create(self, data: UserCreate) -> None:
+        async with aiosqlite.connect(self.db_path) as db:
+            await db.execute('''
+                INSERT INTO users (id, username, age, password, admin, block, user_group, city, address, phone, reg_date, passport)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                data.id,
+                data.username,
+                data.age,
+                data.password,
+                data.admin,
+                data.block,
+                data.user_group,
+                data.city,
+                data.address,
+                data.phone,
+                data.reg_date,
+                data.passport
+            ))
+            rs = await db.commit()
+
+            return rs
+        

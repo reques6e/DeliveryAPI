c11e7e87c17e8bb418e679946577b5561165a5fb
Reques6e <voronin99borja61275y@rambler.ru>
2024-06-24 16:16:20 +0300

    Подключил point в main

--- src/main.py
+++ src/main.py
@@ -6,15 +6,18 @@ from config import Config
 from info.router import router as router_info
 from auth.router import router as router_auth
 from cities.router import router as router_cities
+from point.router import router as router_point
 
 api = FastAPI(
     title='DeliveryAPI By Reques6e',
     version='0.1.0',
     redoc_url=None,
+    description='DeliveryAPI - https://github.com/reques6e/DeliveryAPI'
 )
 
 api.include_router(router_auth)
 api.include_router(router_cities)
+api.include_router(router_point)
 api.include_router(router_info)
 
 if __name__ == "__main__":
